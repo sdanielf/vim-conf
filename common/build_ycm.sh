d3314e26e0d046bdf84b1fcf425309415699d79d
Daniel Francis <francis@sugarlabs.org>
2014-02-09 18:38:54 -0200

    Configure clang in YCM building on ArchLinux. Fixes #4
    Signed-off-by: Daniel Francis <francis@sugarlabs.org>

--- common/build_ycm.sh
+++ common/build_ycm.sh
@@ -2,5 +2,6 @@
 
 mkdir ~/ycm_build
 cd ~/ycm_build
-cmake -G "Unix Makefiles" -DPATH_TO_LLVM_ROOT=~/ycm_temp/llvm_root_dir . ~/.vim/bundle/YouCompleteMe/cpp
+cmake -G "Unix Makefiles" -DPATH_TO_LLVM_ROOT=~/ycm_temp/llvm_root_dir . ~/.vim/bundle/YouCompleteMe/cpp $1
 make ycm_support_libs
+make
